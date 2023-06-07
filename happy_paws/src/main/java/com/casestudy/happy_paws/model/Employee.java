f0c90ffdc6b3ac54ce8071408c48f2fb49f382be
Truongan96dn <124027217+Truongan96dn@users.noreply.github.com>
2023-06-07 14:37:10 +0700

    change

--- happy_paws/src/main/java/com/casestudy/happy_paws/model/Employee.java
+++ happy_paws/src/main/java/com/casestudy/happy_paws/model/Employee.java
@@ -1,6 +1,113 @@
 package com.casestudy.happy_paws.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Employee {
-    private Integer employeeId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
     private String name;
+    private String phone;
+    private String email;
+    private String address;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(name = "date_create", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT now()")
+    @CreationTimestamp
+    private LocalDateTime dateCreate;
+    @Column(name = "date_update", nullable = false, columnDefinition = "TIMESTAMP DEFAULT now()")
+    @UpdateTimestamp
+    private LocalDateTime dateUpdate;
+
+    public Employee() {
+    }
+
+    public Employee(Long employeeId, String name, String phone, String email, String address, User user, LocalDateTime dateCreate, LocalDateTime dateUpdate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.user = user;
+        this.dateCreate = dateCreate;
+        this.dateUpdate = dateUpdate;
+    }
+
+    public Employee(String name, String phone, String email, String address, User user) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.user = user;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getDateCreate() {
+        return dateCreate;
+    }
+
+    public void setDateCreate(LocalDateTime dateCreate) {
+        this.dateCreate = dateCreate;
+    }
+
+    public LocalDateTime getDateUpdate() {
+        return dateUpdate;
+    }
+
+    public void setDateUpdate(LocalDateTime dateUpdate) {
+        this.dateUpdate = dateUpdate;
+    }
 }
